cd0efab81906fe9504db285221d1f71cc9846a18
satoru <satorulogic@gmail.com>
2012-11-23 12:06:51 +0800

    cleanup example codes, make them more pythonic

--- python/algorithms/epsilon_greedy/annealing.py
+++ python/algorithms/epsilon_greedy/annealing.py
@@ -5,31 +5,30 @@ def ind_max(x):
   m = max(x)
   return x.index(m)
 
-class AnnealingEpsilonGreedy():
+class AnnealingEpsilonGreedy(object):
+
   def __init__(self, counts, values):
     self.counts = counts
     self.values = values
-    return
 
   def initialize(self, n_arms):
-    self.counts = [0 for col in range(n_arms)]
-    self.values = [0.0 for col in range(n_arms)]
-    return
+    self.counts = [0] * n_arms
+    self.values = [0.0] * n_arms
 
   def select_arm(self):
     t = sum(self.counts) + 1
     epsilon = 1 / math.log(t + 0.0000001)
-    
+
     if random.random() > epsilon:
       return ind_max(self.values)
     else:
       return random.randrange(len(self.values))
-  
+
   def update(self, chosen_arm, reward):
-    self.counts[chosen_arm] = self.counts[chosen_arm] + 1
-    n = self.counts[chosen_arm]
-    
+    new_count = self.counts[chosen_arm] + 1
+    self.counts[chosen_arm] = new_count
+
     value = self.values[chosen_arm]
-    new_value = ((n - 1) / float(n)) * value + (1 / float(n)) * reward
+    new_value = (((new_count - 1) / float(new_count)) * value +
+                 (1 / float(new_count)) * reward)
     self.values[chosen_arm] = new_value
-    return
